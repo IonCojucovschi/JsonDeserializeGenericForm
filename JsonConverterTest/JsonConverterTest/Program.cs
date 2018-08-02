7c9ad4bde12f0961ff296256be20ed7b67ff471a
DESKTOP-5DFS3EI\IOn <Cojucovschi@gmail.com>
2018-08-02 16:02:58 -0700

    Jrondeserializer example

---
+++ JsonConverterTest/JsonConverterTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using JsonConverterTest.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConverterTest
+{
+    public class DataLoader
+    {
+        public List<SomeObjectTHatUwant> Response = new List<SomeObjectTHatUwant>();///the data you want
+
+        public DataLoader(string uri)
+        {
+            LoadDataAsync(uri);
+        }
+
+
+        async Task LoadDataAsync(string uri)
+        {
+
+            string responseJsonString = null;
+            using (var httpClient = new WebClient())
+            {
+                try
+                {
+                    //Task<HttpResponseMessage> getResponse = httpClient.GetAsync(uri);
+                    // HttpResponseMessage response = await getResponse;
+                    responseJsonString = httpClient.DownloadString(uri);
+                     //responseJsonString = await response.Content.ReadAsStringAsync();
+                    Response = JsonBase<SomeObjectTHatUwant>.ReturnResultsList(responseJsonString);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+    }
+
+
+
+
+    class Program
+    {
+        static string url = "http://readabook.16mb.com/api/allcategory";///this link return an json
+        static List<SomeObjectTHatUwant> Response = new List<SomeObjectTHatUwant>();///the data you want
+        static void Main(string[] args)
+        {
+            DataLoader dt = new DataLoader(url);
+            Response = dt.Response;
+        }
+
+
+         
+
+    }
+}
---
+++ JsonConverterTest/JsonConverterTest/Utils/JsonBase.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConverterTest.Utils
+{
+    public static class JsonBase<T> where T : BaseData<T>
+    {
+        public static List<T> ReturnResultsList(string json)
+        {
+            var returnData = JsonConvert.DeserializeObject<BaseData<T>>(json);
+            return returnData.data;
+        }
+        public static string ReturnMetaData(string json)
+        {
+            var returnData = JsonConvert.DeserializeObject<BaseData<T>>(json);
+            return returnData.metadata;
+        }
+    }
+    public class BaseData<T> where T : class
+    {
+
+        public string metadata { get; set; }
+        public List<T> data { get; set; }
+    }
+    public class SomeObjectTHatUwant : BaseData<SomeObjectTHatUwant>
+    {
+        public string category { get; set; }/// property for my case
+        public string quantity { get; set; }
+
+    }
+}
